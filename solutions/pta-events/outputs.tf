363d83339718c3d9d14497f21ffab1955789d2e8
Stanislav Bahmet <extstbah@pandora.net>
2023-07-18 19:35:59 +0300

    add aks module

--- solutions/pta-events/outputs.tf
+++ solutions/pta-events/outputs.tf
@@ -19,3 +19,26 @@ output "subnet_name" {
 output "subnet_id" {
   value = module.az_subnet.subnet_id
 }
+#AKS
+output "aks_name" {
+  value = module.az_aks.aks_name
+}
+output "aks_id" {
+  value = module.az_aks.aks_id
+}
+output "aks_host" {
+  value = module.az_aks.host
+}
+output "client_certificate" {
+  value = module.az_aks.client_certificate
+}
+output "client_key" {
+  value = module.az_aks.client_key
+}
+output "cluster_ca_certificate" {
+  value = module.az_aks.cluster_ca_certificate
+}
+
+
+
+
