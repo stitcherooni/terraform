95b0c4106d8032dcfce773e5f6dcb261db607d92
Stanislav Bahmet <extstbah@pandora.net>
2023-07-19 14:42:13 +0300

    add aks module

--- modules/az-aks/outputs.tf
+++ modules/az-aks/outputs.tf
@@ -15,21 +15,21 @@ output "node_resource_group" {
 }
 output "host" {
   description = "The Kubernetes cluster server host."
-  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_admin_config.0.host }
+  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_config.0.host }
   sensitive   = true
 }
 output "client_certificate" {
   description = "public certificate used by clients to authenticate to the Kubernetes cluster."
-  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_admin_config.0.client_certificate }
+  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_config.0.client_certificate }
   sensitive   = true
 }
 output "client_key" {
   description = "private key used by clients to authenticate to the Kubernetes cluster"
-  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_admin_config.0.client_key }
+  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_config.0.client_key }
   sensitive   = true
 }
 output "cluster_ca_certificate" {
   description = "public CA certificate used as the root of trust for the Kubernetes cluster."
-  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_admin_config.0.cluster_ca_certificate }
+  value       = { for aks in azurerm_kubernetes_cluster.this : aks.name => aks.kube_config.0.cluster_ca_certificate }
   sensitive   = true
 }
