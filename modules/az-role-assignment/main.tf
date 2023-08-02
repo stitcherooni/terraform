4ede8599a1de29541a7ec88a1d9b5cc4e507da98
Stanislav Bahmet <extstbah@pandora.net>
2023-08-02 17:41:52 +0300

    add terraform modules for private dns and mysql flexible server

--- modules/az-role-assignment/main.tf
+++ modules/az-role-assignment/main.tf
@@ -1,11 +1,12 @@
 resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignment_params
 
-  name                 = lookup(each.value, "name", null)
-  description          = lookup(each.value, "description", null)
-  scope                = lookup(each.value, "scope", null)
-  role_definition_name = lookup(each.value, "role_definition_name", null)
-  principal_id         = lookup(each.value, "principal_id", null)
+  name                             = lookup(each.value, "name", null)
+  description                      = lookup(each.value, "description", null)
+  scope                            = lookup(each.value, "scope", null)
+  role_definition_name             = lookup(each.value, "role_definition_name", null)
+  principal_id                     = lookup(each.value, "principal_id", null)
+  skip_service_principal_aad_check = lookup(each.value, "skip_service_principal_aad_check", false)
 
   timeouts {
     create = "3m"
